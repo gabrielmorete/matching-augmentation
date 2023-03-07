7ec144dc76379de9b1cd50ea30ecd8197f5b4923
gabrielmorete <gmorete7@gmail.com>
2023-03-07 00:19:08 -0500

    added main.h

--- BDS/pegasus/src/main.h
+++ BDS/pegasus/src/main.h
@@ -5,19 +5,19 @@
 	This function receives a LP solution and retuns the edges
 	of a global minimum cut and its value.
 */
-pair<double, vector<Edge> > FindMinCut(double *sol, int n, int m)
+pair<double, vector<Edge> > FindMinCut(double *sol, int n, int m);
 
 /*
 	This function returns a optimum integer solution to MAP.
 	If no solution is found, it returns a all -1 edge map.
 */
-void IntegerSolution(ListGraph::EdgeMap<int> &IntSol)
+void IntegerSolution(ListGraph::EdgeMap<int> &IntSol);
 
 /*
 	This function returns a optimum fractional solution to MAP.
 	If no solution is found, it returns a all -1 edge map.
 */
-void FractionalSolution(ListGraph::EdgeMap<double> &FracSol)
+void FractionalSolution(ListGraph::EdgeMap<double> &FracSol);
 
 
 
