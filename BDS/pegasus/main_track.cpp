cfa7ade9a83518d6d408ac77f45103abc7ceb790
gabrielmorete <gmorete7@gmail.com>
2023-03-07 12:35:50 -0500

    adding start point and append mode

--- BDS/pegasus/main_track.cpp
+++ BDS/pegasus/main_track.cpp
@@ -336,7 +336,7 @@ signed main(int argc, char *argv[]){
 			stdio = 1;
 		else if (s == "-start"){
 			s = argv[i + 1];
-			start = 2;
+			start = to_string((string)s);
 			i++;
 		} else {
 			cout<<"Usage: -stdio -verbose -start n"<<endl;
