1e78df31ae1dd3a331814c0d2dc31d6b68f02b5b
gabrielmorete <gmorete7@gmail.com>
2023-03-11 20:20:51 -0500

    version 2

--- BDS/pegasus/version_2/main.cpp
+++ BDS/pegasus/version_2/main.cpp
@@ -13,7 +13,7 @@
 #include "src/lemon.h"
 #include "src/bds.cpp"
 // #include "src/nauty_reader.cpp"
-// #include "src/stdio_reader.cpp"
+#include "src/stdio_reader.cpp"
 #include "src/main.h"
 
 using namespace std;
