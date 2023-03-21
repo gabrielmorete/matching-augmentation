a989186d95cc6031660d7866dcfbdc4629fa3927
gabrielmorete <gmorete7@gmail.com>
2023-03-21 01:16:26 -0400

    paralelizing solver

--- BDS/pegasus/parallel/src/nauty_reader.cpp
+++ BDS/pegasus/parallel/src/nauty_reader.cpp
@@ -250,7 +250,7 @@ void RunNautyInput(int start, int n_threads = 1){
 	int cnt = 0;
 
     #pragma omp parallel num_threads(n_threads) \
-    shared(cnt, __best_BDS_graph_id, __best_BDS_matching_id, __best_IP_graph_id, __best_IP_matching_id, __best_IP, __best_BDS)
+    shared(cnt, __best_BDS_graph_id, __best_BDS_matching_id, __best_IP_graph_id, __best_IP_matching_id, __best_IP, __best_BDS, log_out)
 	{
 		ListGraph G; // Declare global Graph
 		int my_cnt;
