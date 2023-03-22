485c369717bfefffa916d57d54e63690be36ce5d
gabrielmorete <gmorete7@gmail.com>
2023-03-22 16:26:40 -0400

    update bds

--- BDS/pegasus/parallel/main.cpp
+++ BDS/pegasus/parallel/main.cpp
@@ -358,7 +358,7 @@ void SolveMapInstance(
 	if (sign(FracSol[G.edgeFromId(0)]) == -1)
 		return;
 
-	BDSAlgorithm(cost, FracSol, BDSSol, G);
+	// BDSAlgorithm(cost, FracSol, BDSSol, G);
 
 	// If fractional solution is integral, no need to solve a MIP
 	bool is_integral = 1;
