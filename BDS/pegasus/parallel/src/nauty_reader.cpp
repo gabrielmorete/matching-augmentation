e2629538dbc4359eaadf93a4211ea670cc7e4cac
gabrielmorete <gmorete7@gmail.com>
2023-03-23 19:29:51 -0400

    added tracking

--- BDS/pegasus/parallel/src/nauty_reader.cpp
+++ BDS/pegasus/parallel/src/nauty_reader.cpp
@@ -243,7 +243,7 @@ void PrintLogProgress(int n, int cnt){
 	{ // If you interrupt the algorithm, may be empty
 		ofstream log_progress(to_string(n) + "/log_progress");
 		log_progress << "Last read graph " << cnt << endl; // Careful with this, I'm not using mutex
-		log_progress << "Smallest unprocessed graph" << __cur_graph_id.front() << endl; // Careful with this, I'm not using mutex
+		log_progress << "Smallest unprocessed graph" << __cur_graph_id.top() << endl; // Careful with this, I'm not using mutex
 		log_progress << "Best IP/Frac: " << __best_IP << " g" << __best_IP_graph_id << " matching " << __best_IP_matching_id << endl;
 		log_progress << "Best BDS/Frac: " << __best_BDS << " g" << __best_BDS_graph_id << " matching " << __best_BDS_matching_id << endl;
 		log_progress.close();	
