fa42a552dfe1d5f08dcfa3e0321813a165b86afb
Gabriel Azevedo <gazevedo@cogradmac236.nexus.uwaterloo.ca>
2023-03-24 17:32:25 -0400

    thread affinity

--- BDS/pegasus/parallel/src/nauty_reader.cpp
+++ BDS/pegasus/parallel/src/nauty_reader.cpp
@@ -265,7 +265,7 @@ void RunNautyInput(int start, int n_threads = 1){
 
 	int cnt = 0;
 
-	std::system("export GOMP_CPU_AFFINITY=32-64");
+	// std::system("export GOMP_CPU_AFFINITY=32-64");
 
 
     #pragma omp parallel num_threads(n_threads) \
