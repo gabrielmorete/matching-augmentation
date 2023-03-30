0d8daedd7eca892fddcc8e1eae07fe4253920527
Gabriel Azevedo <gazevedo@cogradmac236.nexus.uwaterloo.ca>
2023-03-30 19:17:19 -0400

    test deg2 conjecture

--- BDS/pegasus/deg2_half/src/nauty_reader.cpp
+++ BDS/pegasus/deg2_half/src/nauty_reader.cpp
@@ -26,6 +26,19 @@ int __cur_graph_thread[100];
 
 #pragma omp threadprivate(__found_feasible, __cur_graph_id)
 
+
+void Print(ListGraph::EdgeMap<int> &cost, ListGraph::EdgeMap<double> &FracSol, ListGraph &G){
+	#pragma omp critical
+	{
+		cout << countNodes(G) << ' ' << countEdges(G) << endl;
+		for (ListGraph::EdgeIt e(G); e != INVALID; ++e){
+	 		cout << G.id(G.u(e)) << " " << G.id(G.v(e)) << " " << cost[e] << ' ' << FracSol[e] << endl;
+		}
+	}
+}
+
+
+
 /*
 	This function calls the LP, IP and BDS algorithms to
 	solve the MAP problem and compares their outputs.
@@ -36,118 +49,31 @@ void SolveCurrentMatching(int matching_id,
 	ListGraph::EdgeMap<int> &cost,
 	GRBModel &frac_model,
 	GRBVar *frac_vars,
-	GRBModel &int_model,
-	GRBVar *int_vars,
 	ListGraph &G){
 
-	ListGraph::EdgeMap<bool> BDSSol(G);
-	ListGraph::EdgeMap<int> IntSol(G);
 	ListGraph::EdgeMap<double> FracSol(G);
+	FractionalSolution(cost, FracSol, frac_model, frac_vars, G);
 
-	SolveMapInstance(cost, FracSol, IntSol, BDSSol, frac_model, frac_vars, int_model, int_vars, G);
+	bool is_half_integral = 0;
+	for (ListGraph::EdgeIt e(G); e != INVALID; ++e)
+		if (sign(FracSol[e]) != 0 and sign(FracSol[e] - 0.5) != 0 sign(FracSol[e] - 1.0) != 0)
+			is_half_integral = 0;
 
-	if (sign(FracSol[G.edgeFromId(0)]) == -1 or IntSol[G.edgeFromId(0)] == -1){
-		#pragma omp critical
-		{
-			ofstream excep(to_string(countNodes(G))+"/exception", std::ios_base::app);
-			excep << "Exception on example g" << __cur_graph_id << " matching id " << matching_id << endl;
-			excep.close();
-		}
-		return;
-	}
+	ListGraph::NodeMap cut_val(G, 0);	
 
+	if (is_half_integral){
+		for (ListGraph::EdgeIt e(G); e != INVALID; ++e){
+			cut_val[G.u(e)] += FracSol[e];
+			cut_val[G.v(e)] += FracSol[e];
+		}	
 
-	int cost_Int = 0;
-	int cost_BDS = 0;
-	double cost_Frac = 0;
-
-	for (ListGraph::EdgeIt e(G); e != INVALID; ++e){
-		int u = G.id(G.u(e));
-		int v = G.id(G.v(e));
-
-		cost_Int +=  IntSol[e] * cost[e];
-		cost_Frac +=  FracSol[e] * cost[e];
-		cost_BDS +=  (int)BDSSol[e] * cost[e];
-	}
-
-	ofstream g_out;
-
-	/* 
-		Found a feasible example, print to file
-	*/
-	if (sign(__IP_divisor * cost_Int - __IP_dividend * cost_Frac) >= 0 or sign(__BDS_divisor * cost_BDS - __BDS_dividend * cost_Frac) > 0){
-		if (__found_feasible == 0){ // First matching found for this graph
-			// create file "g"+cnt
-			g_out.open(to_string(countNodes(G)) + "/g" + to_string(__cur_graph_id));
-			
-			g_out << countNodes(G) <<' ' << countEdges(G) << endl << endl;
-			
-			for (ListGraph::EdgeIt e(G); e != INVALID; ++e)
-				g_out << G.id(G.u(e)) << ' ' << G.id(G.v(e)) << endl;
-			
-			g_out << "----------" << endl << endl;
-		}
-		else
-			g_out.open(to_string(countNodes(G)) + "/g" + to_string(__cur_graph_id), ios::app);
-
-		__found_feasible = 1;
-
-		g_out << matching_id << ": ";
-
-		bool first = 1;
-		for (ListGraph::EdgeIt e(G); e != INVALID; ++e)
-			if (cost[e] == 0){
-				if (!first)
-					g_out << ", ";
-
-				g_out << G.id(G.u(e)) << " " << G.id(G.v(e));
-				first = 0;
+		for (ListGraph::NodeIt v(G); v != INVALID; ++v){
+			if (sign(cut_val[v] - 2.0) == 0){
+				Print(cost, FracSol, G);
+				assert(0);
 			}
-
-		g_out<<endl;	
-
-		g_out << "Frc: " << cost_Frac << " | ";
-		for (ListGraph::EdgeIt e(G); e != INVALID; ++e)
-			g_out << FracSol[e] << ' ';
-		g_out << endl;
-
-		g_out << "Int: " << cost_Int << " | ";
-		for (ListGraph::EdgeIt e(G); e != INVALID; ++e)
-			g_out << IntSol[e] << ' ';
-		g_out << endl;
-
-		g_out << "BDS: " << cost_BDS << " | ";
-		for (ListGraph::EdgeIt e(G); e != INVALID; ++e)
-			g_out << BDSSol[e] << ' ';
-		g_out << endl << endl;
-
-		g_out.close();
-		
-		#pragma omp critical
-		{
-			ofstream log_out(to_string(countNodes(G)) + "/log", ios::app); // clear log file
-			log_out << "Found feasible example g" << __cur_graph_id << " matching id " << matching_id << endl;
-			log_out << "Int/Frc = " << (double) cost_Int/cost_Frac << " BDS/Frc = " << (double) cost_BDS/cost_Frac << endl;
-			log_out << endl;
-			log_out.close();
 		}
 	}
-
-	#pragma omp critical
-	{
-		if (sign((double)cost_Int/cost_Frac - __best_IP) > 0){
-			__best_IP = (double)cost_Int/cost_Frac;
-			__best_IP_graph_id = __cur_graph_id;
-			__best_IP_matching_id = matching_id;
-		}
-
-
-		if (sign((double)cost_BDS/cost_Frac - __best_BDS) > 0){
-			__best_BDS = (double)cost_BDS/cost_Frac;
-			__best_BDS_graph_id = __cur_graph_id;
-			__best_BDS_matching_id = matching_id;
-		}
-	}	
 }
 
 
@@ -161,22 +87,20 @@ void FindAllMatchings(int e_id, int &n, int &m, int &n_matched, int &total_match
 	ListGraph::EdgeMap<int> &cost,
 	GRBModel &frac_model,
 	GRBVar *frac_vars,
-	GRBModel &int_model,
-	GRBVar *int_vars,
 	ListGraph &G){
 
 	if (e_id >= m){
-		SolveCurrentMatching(total_matchings, cost, frac_model, frac_vars, int_model, int_vars, G);
+		SolveCurrentMatching(total_matchings, cost, frac_model, frac_vars, G);
 		return;
 	}
 
 	if (n_matched >= n - 1){ // matching cant increase, prune
-		SolveCurrentMatching(total_matchings, cost, frac_model, frac_vars, int_model, int_vars, G);
+		SolveCurrentMatching(total_matchings, cost, frac_model, frac_vars, G);
 		return;
 	}
 
 	// Case 1 : won't add edge e_id to the matching
-	FindAllMatchings(e_id + 1, n, m, n_matched, total_matchings, matched, cost, frac_model, frac_vars, int_model, int_vars, G); 
+	FindAllMatchings(e_id + 1, n, m, n_matched, total_matchings, matched, cost, frac_model, frac_vars, G); 
 
 	// Case 2 : if possible, will add e_id to the matching
 	ListGraph::Edge e = G.edgeFromId(e_id);
@@ -188,7 +112,7 @@ void FindAllMatchings(int e_id, int &n, int &m, int &n_matched, int &total_match
 		cost[e] = 0;
 		total_matchings++;
 
-		FindAllMatchings(e_id + 1, n, m, n_matched, total_matchings, matched, cost, frac_model, frac_vars, int_model, int_vars, G);
+		FindAllMatchings(e_id + 1, n, m, n_matched, total_matchings, matched, cost, frac_model, frac_vars, G);
 
 		matched[G.u(e)] = 0;
 		matched[G.v(e)] = 0;
@@ -213,16 +137,10 @@ void SolveAllMatchings(ListGraph &G){
 	GRBVar frac_vars[m];
 	BuildFractional(frac_model, frac_vars, G);
 
-	GRBModel int_model(env);
-	GRBVar int_vars[m];
-	BuildIntegral(int_model, int_vars, G);
-	MinimumCut cb = MinimumCut(int_vars, n, m, G);
-	int_model.setCallback(&cb);
-
 	ListGraph::NodeMap<bool> matched(G);
 
 	int total_matchings = 1, n_matched = 0;
-	FindAllMatchings(0, n, m, n_matched, total_matchings, matched, cost, frac_model, frac_vars, int_model, int_vars, G);
+	FindAllMatchings(0, n, m, n_matched, total_matchings, matched, cost, frac_model, frac_vars, G);
 }
 
 
@@ -243,8 +161,6 @@ void PrintLogProgress(int n, int cnt, int last){
 		ofstream log_progress(to_string(n) + "/log_progress");
 		log_progress << "Last read graph " << cnt << endl; // Careful with this, I'm not using mutex
 		log_progress << "Smallest unprocessed graph " << last << endl; // Careful with this, I'm not using mutex
-		log_progress << "Best IP/Frac: " << __best_IP << " g" << __best_IP_graph_id << " matching " << __best_IP_matching_id << endl;
-		log_progress << "Best BDS/Frac: " << __best_BDS << " g" << __best_BDS_graph_id << " matching " << __best_BDS_matching_id << endl;
 		log_progress.close();	
 	}
 }
@@ -307,8 +223,6 @@ void RunNautyInput(int start, int n_threads = 1){
 	else
 		cout << " Running solver with " << "-start " << start << " -threads " << n_threads << endl;
 	
-	cout << " IP gap >= " << __IP_dividend << "/" << __IP_divisor << endl;
-	cout << " BDS gap > " << __BDS_dividend << "/" << __BDS_divisor << endl;
 
 	int cnt = 0;
 
