5cb62c92ac25030ebd8dcf0e2c44d574906ced8f
gabrielmorete <gmorete7@gmail.com>
2023-03-31 00:13:55 -0400

    deg 2 conjecture

--- BDS/pegasus/deg2_half/src/nauty_reader.cpp
+++ BDS/pegasus/deg2_half/src/nauty_reader.cpp
@@ -59,6 +59,9 @@ void SolveCurrentMatching(int matching_id,
 
 	ListGraph::NodeMap<double> cut_val(G, 0);	
 
+	if (is_half_integral)
+		cout<<"oi"<<endl;
+
 	if (is_half_integral){
 		for (ListGraph::EdgeIt e(G); e != INVALID; ++e){
 			cut_val[G.u(e)] += FracSol[e];
