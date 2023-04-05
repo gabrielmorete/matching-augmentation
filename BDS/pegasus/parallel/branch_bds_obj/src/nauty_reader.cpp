2f8f448f65d649482807645f682bc81a1cd122dc
gabrielmorete <gmorete7@gmail.com>
2023-04-05 00:33:32 -0400

    fixing bds obj

--- BDS/pegasus/parallel/branch_bds_obj/src/nauty_reader.cpp
+++ BDS/pegasus/parallel/branch_bds_obj/src/nauty_reader.cpp
@@ -59,7 +59,7 @@ void SolveCurrentMatching(int matching_id,
 		for (int i = 0; i < countNodes(G); i++){
 			cout<<i<<": ";
 			for (auto e : BDS.cover[i])
-				cout<<"("<<BDS.e_v[e]<<" "<<BDS.e_u[e]<<", "<< BDS.memo_edge[e]<<", "<<BDS.in_sol[e]<<") ";
+				cout<<"("<<BDS.e_v[e]<<" "<<BDS.e_u[e]<<", "<<BDS.in_sol[e]<<") ";
 			cout<<endl;
 		}
 
