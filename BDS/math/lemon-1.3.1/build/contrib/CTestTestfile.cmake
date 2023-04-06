40116b3d295c141c08e8b03049c6a0e0b0dbd2bc
Gabriel Azevedo (gazevedo) <gazevedo@pegasus.math.private.uwaterloo.ca>
2023-04-06 14:16:04 -0400

    math

--- BDS/math/lemon-1.3.1/build/contrib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /u/gazevedo/ResearchDocuments/waterloo-bepe/BDS/math/lemon-1.3.1/contrib
-# Build directory: /u/gazevedo/ResearchDocuments/waterloo-bepe/BDS/math/lemon-1.3.1/build/contrib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
