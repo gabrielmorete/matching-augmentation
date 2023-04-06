15b80d7a3b67a18eb3481d28ac860e7929b99eb3
Gabriel Azevedo <gazevedo@cogradmac236.nexus.uwaterloo.ca>
2023-04-05 20:00:42 -0400

    conv comb

--- conv_comb/conv_comb.cpp
+++ conv_comb/conv_comb.cpp
@@ -35,8 +35,8 @@ int sign(double x) { return (x > EPS) - (x < -EPS); }
 /*
 	Combination coefficient
 */
-const double __comb_dividend = 4;
-const double __comb_divisor = 3;
+const double __comb_dividend = 5;
+const double __comb_divisor = 4;
 
 
 using namespace std;
