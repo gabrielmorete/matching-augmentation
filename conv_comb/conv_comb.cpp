e737e8a168b2c3828f1a15bb8d7264cb8931a765
gabrielmorete <gmorete7@gmail.com>
2023-04-09 19:00:01 -0400

    convex comb

--- conv_comb/conv_comb.cpp
+++ conv_comb/conv_comb.cpp
@@ -38,8 +38,8 @@ GRBEnv env = GRBEnv(true);
 	Combination coefficients threshold
 	If run with -coef dividend divisor these values are overwritten
 */
-double __comb_dividend = 5;
-double __comb_divisor = 4;
+double __comb_dividend = 4;
+double __comb_divisor = 3;
 
 class ExtremePoint{
 	private:
