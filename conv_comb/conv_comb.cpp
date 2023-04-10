ea9376ab025911cfdfb38ed552f7f8c205447277
Gabriel Azevedo <gazevedo@cogradmac236.nexus.uwaterloo.ca>
2023-04-10 19:22:39 -0400

    convex comb

--- conv_comb/conv_comb.cpp
+++ conv_comb/conv_comb.cpp
@@ -268,7 +268,7 @@ signed main(int argc, char const *argv[]){
 		int id = 0;
 		for (int i = 1; i < fx.getDim(); i++){
 			int x = fx[i];
-			fx.point[i] = max(0, fx[i] - 0.5);
+			fx.point[i] = max(0.0, fx[i] - 0.5);
 
 			double aux = ConvexComb(sol, fx, int_points);
 			if (aux > wrst){
