8f7b7c4273195791af525e2a15937f0fd318cc72
gabrielmorete <gmorete7@gmail.com>
2023-04-16 01:31:14 -0400

    generators

--- BDS/generatos/run_petersen_blow.py
+++ BDS/generatos/run_petersen_blow.py
@@ -81,10 +81,10 @@ for i in range(30):
 	for j in range(30):
 		adj[i][j] = 0
 
-for x in edges:
-	print(x[0], x[1])
-	adj[x[0]][x[1]] = 1
-	adj[x[1]][x[0]] = 1
+# for x in edges:
+# 	print(x[0], x[1])
+# 	adj[x[0]][x[1]] = 1
+# 	adj[x[1]][x[0]] = 1
 
 
 extra = []
