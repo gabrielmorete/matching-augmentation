b98bff58bc88933f61df98ab4c951b3cb38bd670
gabrielmorete <gmorete7@gmail.com>
2023-04-29 18:09:25 -0400

    new map generator

---
+++ BDS/generatos/k5_c4.py
@@ -0,0 +1,27 @@
+#Generates a blowup pf the K5 in C4
+
+def cycle(id):
+	pre = 4*id
+	return [ [pre, pre + 1, 1], [pre + 1, pre + 2, 1], [pre + 2, pre + 3, 1], [pre + 3, pre, 1]]
+
+edges = []
+
+for i in range(5):
+	edges = edges + cycle(i)
+
+edges.append([0, 4, 0])
+edges.append([1, 8, 0])
+edges.append([2, 12, 0])
+edges.append([3, 16, 0])
+
+edges.append([7, 9, 0])
+edges.append([11, 13, 0])
+edges.append([15, 17, 0])
+edges.append([19, 5, 0])
+
+edges.append([6, 14, 0])
+edges.append([10, 18, 0])
+
+print(20, len(edges))
+for x in sorted(edges):
+	print(x[0], x[1], x[2])
