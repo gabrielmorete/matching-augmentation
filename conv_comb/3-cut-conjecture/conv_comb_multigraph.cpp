eb0e982d9d588889c8d524f78942f9dffdd5fb95
Gabriel Azevedo <gazevedo@cogradmac236.nexus.uwaterloo.ca>
2023-05-25 11:41:07 -0400

    3 cut conj

--- conv_comb/3-cut-conjecture/conv_comb_multigraph.cpp
+++ conv_comb/3-cut-conjecture/conv_comb_multigraph.cpp
@@ -388,7 +388,7 @@ signed main(int argc, char *argv[]){
 	
 
 			for (int i = 0; i < m; i++){
-				// cout << "----------- " << i << endl; 
+				cout << "----------- " << i << endl; 
 				// map<pair<int, int>, bool> in_cut;
 
 				// find_cut(i, G, multi, in_cut);
